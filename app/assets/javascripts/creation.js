9dff4187a82a7bb549fdfbdf62e7d63990765379
Sammilynn <hollow71@students.rowan.edu>
2014-10-23 19:18:32 -0400

    heroku stuff

--- app/assets/javascripts/creation.js
+++ app/assets/javascripts/creation.js
@@ -1,4 +1,4 @@
-//<!--Samantha Holloway Senior Project Oct 2014-->
+//Samantha Holloway Senior Project Oct 2014
 //creation.js
 //Change id to switch css styling for sprite sheet
 //So butterfly pics will change to corresponding radio buttons
@@ -14,11 +14,6 @@ function pictureChange(pic) {
     }
 }
 
-function statChange(pic) {
-
-
-}
-
 //Display correct tooltip for corresponding ability
 function changeToolTip(ability, category) {
     var abilities = document.querySelectorAll(category);
@@ -32,7 +27,7 @@ function changeToolTip(ability, category) {
 
 //Send selected creation choices when user hits submit
 function submitData(){
-        console.log($("input:checked").val() + " is checked!" );
+        //console.log($("input:checked").val() + " is checked!" );
     //Function will search page to collect the user's choices
     //and send data to a another function to be used
     //redirect to play page when complete
